789dcf09e41e910c3baf73fee88ac21d9ca2bbc6
Eirik Haraldsen <eirik_haraldsen@hotmail.com>
2020-11-15 19:18:53 +0100

    Added doc string to utils.py definitions

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 
 def load_dataset(filepath):
-    '''
-    Doc...
-    '''
+    """ Load a dataset from a given filepath.
+    
+    Arguments:
+        filepath: Path to dataset file in json format.
+    """
     obj = []
     with open(filepath, 'r', encoding="UTF-8") as file:
         obj = json.load(file)
@@ -13,9 +15,18 @@ def load_dataset(filepath):
 
 
 def prepare_X_y(train, test):
-    '''
-    doc...
-    '''
+    """ Prepare datasets for feature extraction.
+    
+    Arguments:
+        train: Dataset used for training.
+        test: Dataset used for testing.
+        
+    Returns:
+        X_train: List of all questions in training dataset.
+        y_train: List of all category labels in training dataset.
+        X_test: List of all questions in test dataset.
+        y_test: List of all category labels in test dataset.
+    """
     X_train = []
     y_train = []
     X_test = []
@@ -34,13 +45,15 @@ def prepare_X_y(train, test):
 
 
 def extract_features(train_dataset, test_dataset, file):
-    """Extracts feature vectors from a preprocessed train and test datasets.
+    """Extracts feature vectors from train and test datasets.
     
     Args:
-        train_dataset: List of strings, each consisting of the preprocessed email content. 
-        test_dataset: List of strings, each consisting of the preprocessed email content. 
+        train_dataset: List of strings, each consiting of all training questions 
+        test_dataset: List of strings, each consiting of all test questions  
     
-    Returns: train and test vectors
+    Returns: 
+        train_vectors: frequency feature vectors for training
+        test vectors: frequency feature vectors for test
         
     """
     vectorizer = CountVectorizer()
@@ -52,27 +65,43 @@ def extract_features(train_dataset, test_dataset, file):
     return train_vectors, test_vectors
 
 
-def transform_dataset(test_dataset, file):
+def transform_dataset(dataset, file):
+    """ Extract feature vectors from dataset using vectorizer.
+    
+    Arguments:
+        dataset: Dataset to extract feature vector from.
+        
+    Returns: Feature vector or None
+    """
     with open(file, 'rb') as f:
         return pickle.load(f).transform(test_dataset)
     return None
 
 
-def split_bool_literal_reference(X, y):
-    """
-    doc...
+def split_bool_literal_reference(dataset, labels):
+    """ Seperate dataset into boolean, literal and resource map.
+        Each map uses index in dataset as key and question entity as value.
+    
+    Arguments:
+        dataset: dataset containing question entities.
+        labels: predicted answer category labels.
+        
+    Returns:
+        bool_map: Map with predicted boolean questions.
+        literal_map: Map with predicted literal questions.
+        resource_map: Map with predicted resource questions.
     """
     bool_map = {}
     literal_map = {}
     resource_map = {}
 
     for i in range(len(y)):
-        if y[i] == 'boolean':
-            bool_map[i] = X[i]
-        elif y[i] == 'literal':
-            literal_map[i] = X[i]
-        elif y[i] == 'resource':
-            resource_map[i] = X[i]
+        if labels[i] == 'boolean':
+            bool_map[i] = dataset[i]
+        elif labels[i] == 'literal':
+            literal_map[i] = dataset[i]
+        elif labels[i] == 'resource':
+            resource_map[i] = dataset[i]
             
     return bool_map, literal_map, resource_map
 
